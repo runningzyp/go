d5c88e04d98e1f3ff8e738c90e4d08a8bf8c42d5
yunpeng.zhan <yunpeng.zhan@mail.hypers.com>
2021-05-21 14:01:20 +0800

    更新

--- hello/1_variable.go
+++ hello/1_variable.go
@@ -1,18 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func main()  {
+func main() {
 	var firstName string
-	var secondName,address string
+	var secondName, address string
 	var age int
-	const HTTPStatusOk=200
+	const HTTPStatusOk = 200
 	const (
 		StatusOK              = 0
 		StatusConnectionReset = 1
 		StatusOtherError      = 2
 	)
-	fmt.Println(firstName,secondName,address,age)
-	fmt.Println(StatusOK,StatusConnectionReset,StatusOtherError,HTTPStatusOk)
-
+	var integer8 int8 = 127
+	rune := 'G'
+	//var unsign1 uint= -2
+	//fmt.Println(unsign1)
+	var boolFlag bool = true
+	println(boolFlag)
+	println(rune)
+	println(math.MaxFloat32,math.MaxFloat64)
+	fmt.Println(firstName, secondName, address, age)
+	fmt.Println(StatusOK, StatusConnectionReset, StatusOtherError, HTTPStatusOk)
+	fmt.Println(integer8)
 }
